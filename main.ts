d2b753dbdb10bf83d756221ca6e89c5dfb5b8948
Alberto Sesiliano <aygjiay@gmail.com>
2024-05-22 11:20:33 -0500

    first beta version

--- main.ts
+++ main.ts
@@ -1,81 +1,82 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface LinkToVersePluginSettings {
+	defaultVersion: string;
+	linkTemplate: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: LinkToVersePluginSettings = {
+	defaultVersion: 'default',
+	linkTemplate: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const verseParse = /^((\d+)\s+)?(\w+)\s?((\d+([:\.]\d+)?(-\d+([:\.]\d+)?)?(,\s*)?)+)\s*(\w+)?/i;
+const verseRange = /\d+([:\.]\d+)?(-\d+([:\.]\d+)?)?/ig;
+
+const verseToLink = (selection: string, defaultVersion: string): { book: string, ranges: string[], version: string } => {
+	const parsedVerse = verseParse.exec(selection);
+
+	if (!parsedVerse || !parsedVerse.length) {
+		return { book: '', ranges: [], version: defaultVersion };
+	}
+
+	const book = (parsedVerse[2] ? `${parsedVerse[2]} ` : '') + parsedVerse[3];
+	const rawRanges = parsedVerse[4];
+	const version = parsedVerse[10] || defaultVersion;
+
+	const ranges = rawRanges.match(verseRange)?.map(range => `${range}`) || [];
+
+	return {
+		book,
+		ranges,
+		version
+	};
+}
+
+const formatVerseRange = (parsedVerse: { book: string, ranges: string[], version: string }, range: string, ix: number, defaultVersion: string, linkTemplate: string) => {
+	const bookText = ix === 0 ? parsedVerse.book + ' ' : '';
+	const bookUri = `${parsedVerse.book}+`.replace(' ', '+');
+	const rangeUri = range.replace(':', '.');
+
+	let versionText = '';
+	if (ix === parsedVerse.ranges.length - 1) {
+		versionText = (parsedVerse.version !== defaultVersion ? ` ${parsedVerse.version}` : '')
+	}
+
+	const verseRangeUri = linkTemplate
+		.replace('{{verse}}', `${bookUri}${rangeUri}`)
+		.replace('{{version}}', parsedVerse.version);
+
+	return `[${bookText}${range}${versionText}](${verseRangeUri})`;
+}
+
+
+export default class LinkToVersePlugin extends Plugin {
+	settings: LinkToVersePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: 'create-link-to-bible',
+			name: 'Create link to bible',
+			editorCallback: (editor: Editor, _view: MarkdownView) => {
+				const selection = editor.getSelection();
+				const parsedVerse = verseToLink(selection, this.settings.defaultVersion);
+				const linkToVerse = parsedVerse.ranges.map((range, ix) => formatVerseRange(
+					parsedVerse,
+					range,
+					ix,
+					this.settings.defaultVersion,
+					this.settings.linkTemplate)).join(', ');
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				editor.replaceSelection(linkToVerse);
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new LinkToVerseSettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,43 +92,38 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class LinkToVerseSettingTab extends PluginSettingTab {
+	plugin: LinkToVersePlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: LinkToVersePlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Default version')
+			.setDesc('Which bible version to use when non is given')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your default version')
+				.setValue(this.plugin.settings.defaultVersion)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.defaultVersion = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Link template')
+			.setDesc('A template for the URL to a site shuld have a {{verse}} placeholder and optional a {{version}} placeholder')
+			.addText(text => text
+				.setPlaceholder('Enter your URL template')
+				.setValue(this.plugin.settings.linkTemplate)
+				.onChange(async (value) => {
+					this.plugin.settings.linkTemplate = value;
 					await this.plugin.saveSettings();
 				}));
 	}
